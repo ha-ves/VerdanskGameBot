1e9ee090d2e9d53773339b85e595ac7bd3efcb9f
Haves Irfan <20160532+ha-ves@users.noreply.github.com>
2023-01-04 15:51:44 +0700

    ServerCmd vars combine

--- VerdanskGameBot/Command/CommandServiceVars.cs
+++ VerdanskGameBot/Command/CommandServiceVars.cs
@@ -4,77 +4,82 @@ using System.Collections.Generic;
 
 namespace VerdanskGameBot.Command
 {
-    internal static class ServerCmd
+    internal static partial class ServerCmd
     {
         /// <summary>
         /// Description: <i>Game server commands.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Server"/>
+        /// See Also: <seealso cref="ServerCmd.ServerDesc"/>
         /// </summary>
         internal const string Server = "server";
 
         /// <summary>
         /// Description: <i>Lists all game servers added to watch list.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.List"/>
+        /// See Also: <seealso cref="ServerCmd.ListDesc"/>
         /// </summary>
         internal const string List = "list";
         /// <summary>
         /// Description: <i>Add and show the server status info to this channel.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Add"/>
+        /// See Also: <seealso cref="ServerCmd.AddDesc"/>
         /// </summary>
         internal const string Add = "add";
         /// <summary>
         /// Description: <i>Move the server status info to this channel.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Movehere"/>
+        /// See Also: <seealso cref="ServerCmd.MovehereDesc"/>
         /// </summary>
         internal const string Movehere = "movehere";
         /// <summary>
         /// Description: <i>Remove a server status info from this discord guild/server.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Remove"/>
+        /// See Also: <seealso cref="ServerCmd.RemoveDesc"/>
         /// </summary>
         internal const string Remove = "remove";
         /// <summary>
         /// Description: <i>Change a server status info of this discord guild/server.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Change"/>
+        /// See Also: <seealso cref="ServerCmd.ChangeDesc"/>
         /// </summary>
         internal const string Change = "change";
         /// <summary>
         /// Description: <i>Refresh a server status info of this discord guild/server.</i><br/>
-        /// See Also: <seealso cref="ServerDesc.Refresh"/>
+        /// See Also: <seealso cref="ServerCmd.RefreshDesc"/>
         /// </summary>
         internal const string Refresh = "refresh";
+        /// <summary>
+        /// Description: <i>Refresh a server status info of this discord guild/server.</i><br/>
+        /// See Also: <seealso cref="ServerCmd.AddModal"/>
+        /// </summary>
+        internal const string AddModal = "addmodal";
     }
 
-    internal static class ServerDesc
+    internal static partial class ServerCmd
     {
         /// <summary>
         /// Description for <see cref="ServerCmd.Server"/>
         /// </summary>
-        internal const string Server = "Game server commands.";
+        internal const string ServerDesc = "Game server commands.";
 
         /// <summary>
         /// Description for <see cref="ServerCmd.List"/>
         /// </summary>
-        internal const string List = "Lists all game servers added to watch list.";
+        internal const string ListDesc = "Lists all game servers added to watch list.";
         /// <summary>
         /// Description for <see cref="ServerCmd.Add"/>
         /// </summary>
-        internal const string Add = "Add and show the server status info to this channel.";
+        internal const string AddDesc = "Add and show the server status info to this channel.";
         /// <summary>
         /// Description for <see cref="ServerCmd.Movehere"/>
         /// </summary>
-        internal const string Movehere = "Move the server status info to this channel.";
+        internal const string MovehereDesc = "Move the server status info to this channel.";
         /// <summary>
         /// Description for <see cref="ServerCmd.Remove"/>
         /// </summary>
-        internal const string Remove = "Remove a server status info from this discord guild/server.";
+        internal const string RemoveDesc = "Remove a server status info from this discord guild/server.";
         /// <summary>
         /// Description for <see cref="ServerCmd.Change"/>
         /// </summary>
-        internal const string Change = "Change a server status info of this discord guild/server.";
+        internal const string ChangeDesc = "Change a server status info of this discord guild/server.";
         /// <summary>
         /// Description for <see cref="ServerCmd.Refresh"/>
         /// </summary>
-        internal const string Refresh = "Refresh a server status info of this discord guild/server.";
+        internal const string RefreshDesc = "Refresh a server status info of this discord guild/server.";
     }
 
     internal static class ServerParam
@@ -104,48 +109,48 @@ namespace VerdanskGameBot.Command
             new SlashCommandBuilder
             {
                 Name = ServerCmd.Server,
-                Description = ServerDesc.Server,
+                Description = ServerCmd.ServerDesc,
 
                 Options = new List<SlashCommandOptionBuilder>
                 {
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.List,
-                        Description = ServerDesc.List,
+                        Description = ServerCmd.ListDesc,
                         Type = ApplicationCommandOptionType.SubCommand
                     },
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.Add,
-                        Description = ServerDesc.Add,
+                        Description = ServerCmd.AddDesc,
                         Type = ApplicationCommandOptionType.SubCommand,
                         Options = new List<SlashCommandOptionBuilder> { Servername }
                     },
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.Movehere,
-                        Description = ServerDesc.Movehere,
+                        Description = ServerCmd.MovehereDesc,
                         Type = ApplicationCommandOptionType.SubCommand,
                         Options = new List<SlashCommandOptionBuilder> { Servername }
                     },
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.Remove,
-                        Description = ServerDesc.Remove,
+                        Description = ServerCmd.RemoveDesc,
                         Type = ApplicationCommandOptionType.SubCommand,
                         Options = new List<SlashCommandOptionBuilder> { Servername }
                     },
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.Change,
-                        Description = ServerDesc.Change,
+                        Description = ServerCmd.ChangeDesc,
                         Type = ApplicationCommandOptionType.SubCommand,
                         Options = new List<SlashCommandOptionBuilder> { Servername }
                     },
                     new SlashCommandOptionBuilder
                     {
                         Name = ServerCmd.Refresh,
-                        Description = ServerDesc.Refresh,
+                        Description = ServerCmd.RefreshDesc,
                         Type = ApplicationCommandOptionType.SubCommand,
                         Options = new List<SlashCommandOptionBuilder> { Servername }
                     }
